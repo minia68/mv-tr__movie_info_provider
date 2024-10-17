03d0d7f6bb2291a2deadc13eee00b5e4c24dbde3
ryzhkovsm <ryzhkovsm@gmail.com>
2024-10-17 21:11:51 +0300

    export kpun_rating_datasource

--- lib/movie_info_provider.dart
+++ lib/movie_info_provider.dart
@@ -6,4 +6,5 @@ export 'src/tracker/nnmclub/nnmclub_tracker_datasource.dart';
 export 'src/tracker/tracker_datasource.dart';
 export 'src/mdb/tmdb/tmdb_datasource.dart';
 export 'src/rating/dummy_rating_datasource.dart';
+export 'src/rating/kpun_rating_datasource.dart';
 export 'src/html_page_provider.dart';
